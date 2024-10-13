83ecdb09ee5be9bb112bcc84092208696691ec01
justinnonso05 <chinonsoali2005@gmail.com>
2024-10-13 13:55:29 +0100

    static files

--- Blog/settings.py
+++ Blog/settings.py
@@ -167,8 +167,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'Blog/')
-STATIC_ROOT = BASE_DIR/'assets'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATIC_ROOT = BASE_DIR/'assets'
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type

62145bed07c32605b2841a2ecbed5f5b469ed3df
justinnonso05 <chinonsoali2005@gmail.com>
2024-10-13 18:07:45 +0100

    test

--- Blog/settings.py
+++ Blog/settings.py
@@ -44,7 +44,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lg8=-sz27ko7!f@lv+x-z8)lzro3tq1j!7r=aw$3pobqstk1ta'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'TRUE') == 'TRUE'
+# DEBUG = os.environ.get('DEBUG', 'TRUE') == 'TRUE'
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ["https://blog-backend-wxhe.onrender.com"]
